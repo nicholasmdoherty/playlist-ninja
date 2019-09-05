8611bb66e70df293ed2cfbfbf026ee974da4d844
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-04 20:53:18 -0400

    no selected playlist CTA

--- src/modules/playlist-builder/PlaylistBuilder.jsx
+++ src/modules/playlist-builder/PlaylistBuilder.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
+import { Col } from "react-bootstrap";
 import { connect } from "react-redux";
+import Spacer from "../../common/components/Spacer";
+import "./playlist-builder.css";
 
 class PlaylistBuilder extends Component {
   render() {
-    return <div></div>;
+    let { selectedPlaylist } = this.props;
+
+    if (!selectedPlaylist) {
+      return (
+        <div id="playlist-builder-wrapper">
+          <Col xs={12}>
+            <Spacer percentage={3} />
+            <div className="w-100 h-100 no-selected-playlist">
+              <h1 className="display-4"> No playlist selected... </h1>
+              <br />
+              <p className="lead">
+                Would you like to <a href="">create a new one?</a>
+              </p>
+              <p className="lead"> or </p>
+              <p className="lead">
+                <a href="">edit an existing one?</a>
+              </p>
+            </div>
+          </Col>
+        </div>
+      );
+    }
+
+    return (
+      <div id="playlist-builder-wrapper">
+        <h1>Selected playlist: {selectedPlaylist.name} </h1>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    api: state.api ? state.api.spotifyApi : null
+    api: state.api ? state.api.spotifyApi : null,
+    selectedPlaylist: state.playlist ? state.playlist.selectedPlaylist : null
   };
 };
 

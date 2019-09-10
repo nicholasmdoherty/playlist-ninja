193ff7a52db7f65eadf4d12eb4099b11a7cc6472
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-10 16:44:52 -0400

    re-design table to work better on mobile

--- src/modules/playlist-builder/subcomponents/TrackTable.jsx
+++ src/modules/playlist-builder/subcomponents/TrackTable.jsx
@@ -4,25 +4,25 @@ import { Table, Button } from "react-bootstrap";
 class TrackTable extends Component {
   render() {
     return (
-      <Table responsive striped bordered hover>
-        <thead>
-          <th style={{ width: "50%" }}> Title </th>
-          <th style={{ width: "30%" }}> Artist </th>
-          <th style={{ width: "20%" }}> Actions </th>
-        </thead>
+      <Table responsive striped hover>
+        <thead></thead>
         <tbody>
           {this.props.tracks.map(playlistTrack => {
             return (
               <tr>
-                <td>{playlistTrack.track.name} </td>
-                <td>
-                  {playlistTrack.track.artists.map((artist, index) => {
-                    return index === playlistTrack.track.artists.length - 1
-                      ? artist.name
-                      : artist.name + ", ";
-                  })}
+                <td className="word-wrap">
+                  <p className="track-table-title">
+                    {playlistTrack.track.name}
+                  </p>
+                  <p className="track-table-font ">
+                    {playlistTrack.track.artists.map((artist, index) => {
+                      return index === playlistTrack.track.artists.length - 1
+                        ? artist.name
+                        : artist.name + ", ";
+                    })}
+                  </p>
                 </td>
-                <td className="text-right">
+                <td className="text-right word-wrap">
                   {this.props.tracksInPlaylist ? (
                     <Button variant="danger" size="sm">
                       -
